4a9ad3a2988576dda265e14aeea925af6a461e8f
luoyesiqiu <9801756+luoyesiqiu@users.noreply.github.com>
2022-12-26 22:24:44 +0800

    优化包装函数

--- shell/src/main/cpp/JniWrapper.cpp
+++ shell/src/main/cpp/JniWrapper.cpp
@@ -20,8 +20,13 @@ jobject W_NewObject(JNIEnv* env,jclass klass,const char* sig,...){
     return obj;
 }
 
-jobject W_CallObjectMethod(JNIEnv* env,jclass klass,jobject obj,const char* name,const char* sig,...){
-    if(nullptr == env || nullptr == klass || nullptr == obj || nullptr == name || nullptr == sig ){
+jobject W_CallObjectMethod(JNIEnv* env,jobject obj,const char* name,const char* sig,...){
+    if(nullptr == env || nullptr == obj || nullptr == name || nullptr == sig ){
+        return nullptr;
+    }
+    jclass klass = env->GetObjectClass(obj);
+    if(env->ExceptionCheck() || klass == nullptr){
+        env->ExceptionClear();
         return nullptr;
     }
     va_list arg;
@@ -31,48 +36,66 @@ jobject W_CallObjectMethod(JNIEnv* env,jclass klass,jobject obj,const char* name
     va_end(arg);
     if(env->ExceptionCheck()){
         env->ExceptionClear();
+        W_DeleteLocalRef(env,klass);
         return nullptr;
     }
 
+    W_DeleteLocalRef(env,klass);
+
     return retObj;
 }
-jint W_CallIntMethod(JNIEnv* env,jclass klass,jobject obj,const char* name,const char* sig,...){
-    if(nullptr == env || nullptr == klass || nullptr == obj || nullptr == name || nullptr == sig ){
-        return 0;
+jint W_CallIntMethod(JNIEnv* env,jobject obj,const char* name,const char* sig,jint defVal,...){
+    if(nullptr == env || nullptr == obj || nullptr == name || nullptr == sig ){
+        return defVal;
+    }
+    jclass klass = env->GetObjectClass(obj);
+    if(env->ExceptionCheck() || klass == nullptr){
+        env->ExceptionClear();
+        return defVal;
     }
     va_list arg;
-    va_start(arg,sig);
+    va_start(arg,defVal);
     jmethodID jmethodId = env->GetMethodID(klass,name,sig);
     jint ret = env->CallIntMethodV(obj,jmethodId,arg);
     va_end(arg);
     if(env->ExceptionCheck()){
         env->ExceptionClear();
-        return 0;
+        W_DeleteLocalRef(env,klass);
+        return defVal;
     }
-
     return ret;
 }
 
-jboolean W_CallBooleanMethod(JNIEnv* env,jclass klass,jobject obj,const char* name,const char* sig,...){
-    if(nullptr == env || nullptr == klass || nullptr == obj || nullptr == name || nullptr == sig ){
-        return JNI_FALSE;
+jboolean W_CallBooleanMethod(JNIEnv* env,jobject obj,const char* name,const char* sig,jboolean defVal,...){
+    if(nullptr == env || nullptr == obj || nullptr == name || nullptr == sig ){
+        return defVal;
+    }
+    jclass klass = env->GetObjectClass(obj);
+    if(env->ExceptionCheck() || klass == nullptr){
+        env->ExceptionClear();
+        return defVal;
     }
     va_list arg;
-    va_start(arg,sig);
+    va_start(arg,defVal);
     jmethodID jmethodId = env->GetMethodID(klass,name,sig);
     jboolean ret = env->CallBooleanMethodV(obj,jmethodId,arg);
     va_end(arg);
     if(env->ExceptionCheck()){
         env->ExceptionClear();
-        return JNI_FALSE;
+        W_DeleteLocalRef(env,klass);
+        return defVal;
     }
-
+    W_DeleteLocalRef(env,klass);
     return ret;
 }
 
-
-void W_CallVoidMethod(JNIEnv* env,jclass klass,jobject obj,const char* name,const char* sig,...){
-    if(nullptr == env || nullptr == klass || nullptr == obj || nullptr == name || nullptr == sig ){
+void W_CallVoidMethod(JNIEnv* env,jobject obj,const char* name,const char* sig,...){
+    if(nullptr == env || nullptr == obj || nullptr == name || nullptr == sig ){
+        return;
+    }
+    jclass klass = env->GetObjectClass(obj);
+    if(env->ExceptionCheck() || klass == nullptr){
+        env->ExceptionClear();
         return;
     }
     va_list arg;
@@ -82,7 +105,9 @@ void W_CallVoidMethod(JNIEnv* env,jclass klass,jobject obj,const char* name,cons
     va_end(arg);
     if(env->ExceptionCheck()){
         env->ExceptionClear();
+        W_DeleteLocalRef(env,klass);
     }
+    W_DeleteLocalRef(env,klass);
 }
 
 void W_DeleteLocalRef(JNIEnv* env,jobject obj){
