259c0a81d4ff5066cab22b47c65f16346c32ed36
luoyesiqiu <9801756+luoyesiqiu@users.noreply.github.com>
2023-03-02 22:29:50 +0800

    修正so文件目录获取

--- shell/src/main/java/com/luoyesiqiu/shell/util/SystemUtils.java
+++
@@ -1,9 +0,0 @@
-package com.luoyesiqiu.shell.util;
-
-import android.os.Build;
-
-public class SystemUtils {
-    public static boolean is64Bits(){
-        return Build.SUPPORTED_64_BIT_ABIS.length != 0;
-    }
-}
