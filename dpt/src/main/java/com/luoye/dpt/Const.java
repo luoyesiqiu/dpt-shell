77e498a869886f9b225d687ffc248a94ce7dc78b
luoyesiqiu <9801756+luoyesiqiu@users.noreply.github.com>
2023-04-05 10:21:30 +0800

    自定义线程名

--- dpt/src/main/java/com/luoye/dpt/Const.java
+++ dpt/src/main/java/com/luoye/dpt/Const.java
@@ -7,6 +7,7 @@ public class Const {
     public static final String STORE_PASSWORD = "android";
     public static final String KEY_PASSWORD = "android";
     public static final String KEY_ALIAS = "androiddebugkey";
+    public static final String DEFAULT_THREAD_NAME = "dpt";
 
     public static final String ROOT_OF_OUT_DIR = System.getProperty("java.io.tmpdir");
     public static final String PROXY_APPLICATION_NAME = "com.luoyesiqiu.shell.ProxyApplication";
