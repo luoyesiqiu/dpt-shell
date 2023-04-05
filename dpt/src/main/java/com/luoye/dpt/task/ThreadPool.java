8257be64e602e2f07a7fa031c8f9c2a82927d7b8
luoyesiqiu <yanwendream@163.com>
2023-04-05 10:21:30 +0800

    自定义线程名

--- dpt/src/main/java/com/luoye/dpt/task/ThreadPool.java
+++ dpt/src/main/java/com/luoye/dpt/task/ThreadPool.java
@@ -1,6 +1,9 @@
 package com.luoye.dpt.task;
 
+import com.luoye.dpt.Const;
+
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -14,7 +17,8 @@ public class ThreadPool {
             Runtime.getRuntime().availableProcessors() * 2,
             0L,
             TimeUnit.MILLISECONDS,
-            new LinkedBlockingQueue<>());
+            new LinkedBlockingQueue<>(),
+            new CustomThreadFactory());
     private ThreadPool(){
 
     }
@@ -23,6 +27,15 @@ public class ThreadPool {
             sInst = new ThreadPool();
         }
         return sInst;
+    }
+
+    public static class CustomThreadFactory implements ThreadFactory {
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r);
+            t.setName(Const.DEFAULT_THREAD_NAME + "-" + t.getId());
+            return t;
+        }
     }
 
     public void execute(Runnable task){
