1d1f4562ab183c5a4e0c50b7016c9d61e827a458
luoyesiqiu <9801756+luoyesiqiu@users.noreply.github.com>
2023-05-27 23:34:40 +0800

    优化日志

--- shell/src/main/cpp/common/dpt_log.h
+++ shell/src/main/cpp/common/dpt_log.h
@@ -14,12 +14,6 @@
 #define DLOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
 #define DLOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG,__VA_ARGS__)
 #define DLOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG,__VA_ARGS__)
-#else
-#define DLOGI(...)
-#define DLOGD(...)
-#define DLOGE(...)
-#define DLOGW(...)
-#endif
 
 #ifdef NOICE_LOG
 #define NLOG(...) { \
@@ -28,5 +22,13 @@
 #else
 #define NLOG(...)
 #endif
+
+#else
+#define DLOGI(...)
+#define DLOGD(...)
+#define DLOGE(...)
+#define DLOGW(...)
+#endif
+
 
 #endif //DPT_DPT_LOG_H
