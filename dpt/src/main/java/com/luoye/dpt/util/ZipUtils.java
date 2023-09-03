97de44d60d211e34e2f1ca4324e95a8b6ef5f5dd
hlx <hanliuxin5@users.noreply.github.com>
2023-09-03 18:10:47 +0800

    Update ZipUtils.java
    记录压缩方法，并且在重新组包时使用正确的压缩方法。

--- dpt/src/main/java/com/luoye/dpt/util/ZipUtils.java
+++ dpt/src/main/java/com/luoye/dpt/util/ZipUtils.java
@@ -4,8 +4,9 @@ import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.FileHeader;
 import net.lingala.zip4j.model.ZipParameters;
-import net.lingala.zip4j.model.enums.CompressionMethod;
 import net.lingala.zip4j.model.enums.CompressionLevel;
+import net.lingala.zip4j.model.enums.CompressionMethod;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -23,19 +24,24 @@ import java.util.zip.ZipInputStream;
 public class ZipUtils {
 
     private static final String RENAME_SUFFIX = ".renamed";
+    private static final HashMap<String, CompressionMethod> compressedLevelMap = new HashMap<>();
+
     /**
      * 读取资源
      */
-    public static void readResourceFromRuntime(String resourcePath,String distPath) throws IOException{
-        InputStream inputStream = ZipUtils.class.getClassLoader().getResourceAsStream(resourcePath);
-        if(inputStream == null){
+    public static void readResourceFromRuntime(String resourcePath, String distPath) throws IOException {
+        InputStream inputStream = ZipUtils.class.getClassLoader()
+                                                .getResourceAsStream(resourcePath);
+        if (inputStream == null) {
             throw new IOException("cannot get resource:" + resourcePath);
         }
         BufferedInputStream in = null;
         BufferedOutputStream out = null;
         File distFile = new File(distPath);
-        if(!distFile.getParentFile().exists()){
-            distFile.getParentFile().mkdirs();
+        if (!distFile.getParentFile()
+                     .exists()) {
+            distFile.getParentFile()
+                    .mkdirs();
         }
         try {
             in = new BufferedInputStream(inputStream);
@@ -53,30 +59,28 @@ public class ZipUtils {
             IoUtils.close(in);
         }
     }
+
     /**
      * 压缩文件/文件夹为apk
      */
-    public static void compressToApk(String srcDir,String destFile)  {
-        ZipFile zipFile =  null;
+    public static void compressToApk(String srcDir, String destFile) {
+        ZipFile zipFile = null;
         try {
-             zipFile = new ZipFile(destFile);
+            zipFile = new ZipFile(destFile);
             File dir = new File(srcDir);
             File[] list = dir.listFiles();
-            if(list == null){
+            if (list == null) {
                 return;
             }
             ZipParameters zipParameters = new ZipParameters();
-            for(File f : list){
-                zipParameters.setCompressionMethod(CompressionMethod.DEFLATE);
-                zipParameters.setCompressionLevel(CompressionLevel.ULTRA);
-                if(f.isDirectory()){
-                    zipFile.addFolder(f.getAbsoluteFile(),zipParameters);
+            for (File f : list) {
+                if (compressedLevelMap.containsKey(f.getName())) {
+                    zipParameters.setCompressionMethod(compressedLevelMap.get(f.getName()));
                 }
-                else{
-                    if(f.getName().equals("resources.arsc")) {
-                        zipParameters.setCompressionMethod(CompressionMethod.STORE);
-                    }
-                    zipFile.addFile(f.getAbsoluteFile(),zipParameters);
+                if (f.isDirectory()) {
+                    zipFile.addFolder(f.getAbsoluteFile(), zipParameters);
+                } else {
+                    zipFile.addFile(f.getAbsoluteFile(), zipParameters);
                 }
             }
 
@@ -91,21 +95,21 @@ public class ZipUtils {
                 }
             }
 
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             IoUtils.close(zipFile);
         }
     }
 
-    private static void writeZipEntry(ZipInputStream zipInputStream,String targetFilePath){
+    private static void writeZipEntry(ZipInputStream zipInputStream, String targetFilePath) {
         FileOutputStream fos = null;
         try {
             File targetFile = new File(targetFilePath);
-            if(!targetFile.getParentFile().exists()){
-                targetFile.getParentFile().mkdirs();
+            if (!targetFile.getParentFile()
+                           .exists()) {
+                targetFile.getParentFile()
+                          .mkdirs();
             }
             fos = new FileOutputStream(targetFile);
             int len = 0;
@@ -113,11 +117,9 @@ public class ZipUtils {
             while ((len = zipInputStream.read(buf)) != -1) {
                 fos.write(buf, 0, len);
             }
-        }
-        catch (IOException e){
-            LogUtils.error("writeZipEntry err = %s",e);
-        }
-        finally {
+        } catch (IOException e) {
+            LogUtils.error("writeZipEntry err = %s", e);
+        } finally {
             IoUtils.close(fos);
         }
     }
@@ -125,9 +127,9 @@ public class ZipUtils {
     /**
      * 解压apk所有文件
      */
-    public static void extractAPK(String zipFilePath, String destDir)  {
+    public static void extractAPK(String zipFilePath, String destDir) {
         ZipInputStream zipInputStream = null;
-        Map<String,Integer> zipEntryNameMap = new HashMap<>();
+        Map<String, Integer> zipEntryNameMap = new HashMap<>();
         try {
             zipInputStream = new ZipInputStream(new FileInputStream(zipFilePath));
             ZipEntry zipEntry = null;
@@ -136,24 +138,25 @@ public class ZipUtils {
 
                 String zipEntryName = zipEntry.getName();
 
+                CompressionMethod compressionMethod = CompressionMethod.getCompressionMethodFromCode(zipEntry.getMethod());
+
+                compressedLevelMap.put(zipEntryName, compressionMethod);
+
                 String lowerCase = zipEntryName.toLowerCase(Locale.US);
                 String finalFileName = zipEntryName;
-                if(zipEntryNameMap.get(lowerCase) != null){
+                if (zipEntryNameMap.get(lowerCase) != null) {
                     int num = zipEntryNameMap.get(lowerCase) + 1;
                     finalFileName = zipEntryName + RENAME_SUFFIX + num;
-                    zipEntryNameMap.put(lowerCase ,num);
-                    LogUtils.noisy("extract file rename: %s -> %s",zipEntryName,finalFileName);
+                    zipEntryNameMap.put(lowerCase, num);
+                } else {
+                    zipEntryNameMap.put(lowerCase, 0);
                 }
-                else{
-                    zipEntryNameMap.put(lowerCase,0);
-                }
-                writeZipEntry(zipInputStream,destDir + File.separator + finalFileName);
+                writeZipEntry(zipInputStream, destDir + File.separator + finalFileName);
 
             }
-        }
-        catch (Exception e){
-        }
-        finally {
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
             IoUtils.close(zipInputStream);
         }
     }
@@ -161,13 +164,12 @@ public class ZipUtils {
     /**
      * 解压单个文件
      */
-    public static void extractFile(String zipFilePath,String fileName, String destDir)  {
+    public static void extractFile(String zipFilePath, String fileName, String destDir) {
         ZipFile zipFile = new ZipFile(zipFilePath);
         try {
             FileHeader fileHeader = zipFile.getFileHeader(fileName);
-            zipFile.extractFile(fileHeader,destDir);
-        }
-        catch (ZipException e){
+            zipFile.extractFile(fileHeader, destDir);
+        } catch (ZipException e) {
             e.printStackTrace();
         }
     }
@@ -176,22 +178,20 @@ public class ZipUtils {
     /**
      * 正常压缩文件
      */
-    public static void compress(List<File> files,String destFile)  {
+    public static void compress(List<File> files, String destFile) {
         ZipFile zipFile = new ZipFile(destFile);
-        if(files == null){
+        if (files == null) {
             return;
         }
         try {
-            for(File f : files){
-                if(f.isDirectory()){
+            for (File f : files) {
+                if (f.isDirectory()) {
                     zipFile.addFolder(f.getAbsoluteFile());
-                }
-                else{
+                } else {
                     zipFile.addFile(f.getAbsoluteFile());
                 }
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
