aa0ad8edc992f931cd8f743c0223ad79cb321ed6
luoyesiqiu <9801756+luoyesiqiu@users.noreply.github.com>
2024-04-08 22:15:41 +0800

    Add detect frida logic

--- shell/src/main/cpp/dpt_risk.cpp
+++ shell/src/main/cpp/dpt_risk.cpp
@@ -17,3 +17,31 @@ void junkCodeDexProtect(JNIEnv *env) {
         crash();
     }
 }
+
+[[noreturn]] void *detectFridaOnThread(__unused void *args) {
+    while (true) {
+        int frida_so_count = find_in_maps(1,"frida-agent.so");
+        if(frida_so_count > 0) {
+            DLOGD("detectFridaOnThread found frida so");
+            crash();
+        }
+        int frida_thread_count = find_in_threads_list(4
+                ,"pool-frida"
+                ,"gmain"
+                ,"gdbus"
+                ,"gum-js-loop");
+
+        if(frida_thread_count >= 2) {
+            DLOGD("detectFridaOnThread found frida threads");
+            crash();
+        }
+        DLOGD("detectFridaOnThread pass");
+        sleep(10);
+    }
+}
+
+
+void detectFrida() {
+    pthread_t t;
+    pthread_create(&t, nullptr,detectFridaOnThread,nullptr);
+}
--- shell/src/main/cpp/dpt_util.h
+++ shell/src/main/cpp/dpt_util.h
@@ -5,25 +5,29 @@
 #ifndef DPT_DPT_UTIL_H
 #define DPT_DPT_UTIL_H
 #include <string>
-#include <jni.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <libgen.h>
 #include <string.h>
 #include <unistd.h>
-#include <android/asset_manager.h>
-#include <android/asset_manager_jni.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <stdlib.h>
+#include <sys/prctl.h>
+#include <dirent.h>
+#include <jni.h>
+#include <android/asset_manager.h>
+#include <android/asset_manager_jni.h>
+
 #include <minizip-ng/mz_zip.h>
 #include <minizip-ng/mz_strm_mem.h>
 #include <minizip-ng/mz.h>
-#include <stdlib.h>
-#include <sys/prctl.h>
+
+#include "dpt_jni.h"
 #include "common/dpt_log.h"
 #include "common/dpt_macro.h"
-#include "dpt_jni.h"
+
 #include "reflect/android_app_ActivityThread.h"
 #include "reflect/android_content_pm_ApplicationInfo.h"
 #include "reflect/java_lang_Class.h"
@@ -45,7 +49,8 @@ void appendLog(const char* log);
 void hexDump(const char* name,const void* data, size_t size);
 void load_zip(const char* zip_file_path,void **zip_addr,off_t *zip_size);
 void *read_zip_file_entry(void* zip_addr,off_t zip_size,const char* entry_name,int64_t *entry_size);
-int find_in_maps(const char* find_name,pointer_t *start,pointer_t *end,char *full_path);
+int find_in_maps(int count,...);
+int find_in_threads_list(int count,...);
 const char* find_symbol_in_elf_file(const char *elf_file,int keyword_count,...);
 void readPackageName(char *packageName,size_t max_len);
 void getClassName(JNIEnv *env,jobject obj,char *destClassName);
