825fa138a0f9d1ed2d63ee89a962255bfc320183
luoyesiqiu <yanwendream@163.com>
2024-06-23 08:11:18 +0800

    Enhanced anti debugger

--- shell/src/main/cpp/dpt_risk.cpp
+++ shell/src/main/cpp/dpt_risk.cpp
@@ -4,11 +4,24 @@
 
 #include "dpt_risk.h"
 
-inline void crash() {
-    Dl_info info;
-    dladdr((const void *)junkCodeDexProtect, &info);
-    void (*func)() = (void (*)())info.dli_fbase;
-    func();
+void crash() {
+#ifdef __aarch64__
+    asm volatile(
+            "mov x30,#0\t\n"
+            );
+#elif __arm__
+    asm volatile(
+        "mov lr,#0\t\n"
+    );
+#elif __i386__
+    asm volatile(
+            "ret\t\n"
+            );
+#elif __x86_64__
+    asm volatile(
+            "pop %rbp\t\n"
+    );
+#endif
 }
 
 void junkCodeDexProtect(JNIEnv *env) {
@@ -44,4 +57,23 @@ void junkCodeDexProtect(JNIEnv *env) {
 void detectFrida() {
     pthread_t t;
     pthread_create(&t, nullptr,detectFridaOnThread,nullptr);
+}
+
+void doPtrace() {
+    __unused int ret = sys_ptrace(PTRACE_TRACEME,0,0,0);
+    DLOGD("doPtrace result: %d",ret);
+}
+
+void *protectProcessOnThread(__unused void *args) {
+    int pid = wait(NULL);
+    if(pid > 0) {
+        DLOGD("%s detect child process %d exit!", __FUNCTION__, pid);
+        crash();
+    }
+    return nullptr;
+}
+
+void protectChildProcess(int pid) {
+    pthread_t t;
+    pthread_create(&t, nullptr,protectProcessOnThread,&pid);
 }
