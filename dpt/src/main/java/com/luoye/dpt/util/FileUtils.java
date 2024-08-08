6adc62a42afa24f51a11c8206f30e871e94ca3b8
Jimmyzhou <jimmyzhou@yeahka.com>
2024-08-08 10:54:22 +0800

    feat-mergeDex:merge dex.zip with proxyDex

--- dpt/src/main/java/com/luoye/dpt/util/FileUtils.java
+++ dpt/src/main/java/com/luoye/dpt/util/FileUtils.java
@@ -1,8 +1,11 @@
 package com.luoye.dpt.util;
 
 import java.io.File;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.zip.Adler32;
 
 /**
  * @author luoyesiqiu
@@ -80,4 +83,79 @@ public class FileUtils {
     public static String getExecutablePath(){
         return System.getProperty("user.dir");
     }
+
+    /**
+     * 修改dex头，CheckSum 校验码
+     *
+     * @param dexBytes
+     */
+    public static void fixCheckSumHeader(byte[] dexBytes) {
+        Adler32 adler = new Adler32();
+        adler.update(dexBytes, 12, dexBytes.length - 12);//从12到文件末尾计算校验码
+        long value = adler.getValue();
+        int va = (int) value;
+        byte[] newcs = intToByte(va);
+        //高位在前，低位在前掉个个
+        byte[] recs = new byte[4];
+        for (int i = 0; i < 4; i++) {
+            recs[i] = newcs[newcs.length - 1 - i];
+        }
+        System.arraycopy(recs, 0, dexBytes, 8, 4);//效验码赋值（8-11）
+    }
+
+
+    /**
+     * int 转byte[]
+     *
+     * @param number
+     * @return
+     */
+    public static byte[] intToByte(int number) {
+        byte[] b = new byte[4];
+        for (int i = 3; i >= 0; i--) {
+            b[i] = (byte) (number % 256);
+            number >>= 8;
+        }
+        return b;
+    }
+
+    /**
+     * 修改dex头 sha1值
+     *
+     * @param dexBytes
+     * @throws NoSuchAlgorithmException
+     */
+    public static void fixSHA1Header(byte[] dexBytes)
+            throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        md.update(dexBytes, 32, dexBytes.length - 32);//从32为到结束计算sha--1
+        byte[] newdt = md.digest();
+        System.arraycopy(newdt, 0, dexBytes, 12, 20);//修改sha-1值（12-31）
+        //输出sha-1值，可有可无
+        String hexstr = "";
+        for (int i = 0; i < newdt.length; i++) {
+            hexstr += Integer.toString((newdt[i] & 0xff) + 0x100, 16)
+                    .substring(1);
+        }
+        //System.out.println(hexstr);
+    }
+
+    /**
+     * 修改dex头 file_size值
+     *
+     * @param dexBytes
+     */
+    public static void fixFileSizeHeader(byte[] dexBytes) {
+        //新文件长度
+        byte[] newfs = intToByte(dexBytes.length);
+        System.out.println("fixFileSizeHeader ===== size : " + dexBytes.length);
+        byte[] refs = new byte[4];
+        //高位在前，低位在前掉个个
+        for (int i = 0; i < 4; i++) {
+            refs[i] = newfs[newfs.length - 1 - i];
+            //System.out.println(Integer.toHexString(newfs[i]));
+        }
+        System.arraycopy(refs, 0, dexBytes, 32, 4);//修改（32-35）
+    }
+
 }
