63cad2055edef55f2c31e0fc2f657c7e8b158a16
luoyesiqiu <9801756+luoyesiqiu@users.noreply.github.com>
2025-01-01 20:49:48 +0800

    Fix release version crash(#82)

--- shell/src/main/java/com/luoyesiqiu/shell/Global.java
+++ shell/src/main/java/com/luoyesiqiu/shell/Global.java
@@ -1,5 +1,7 @@
 package com.luoyesiqiu.shell;
 
+import androidx.annotation.Keep;
+
 /**
  * @author luoyesiqiu
  */
@@ -8,7 +10,9 @@ public class Global {
     public static final String ZIP_LIB_DIR = "vwwwwwvwww";
     public static final String LIB_DIR = "dpt-libs";
     public static final String SHELL_SO_NAME = "libdpt.so";
+    @Keep
     public volatile static boolean sIsReplacedClassLoader = false;
+    @Keep
     public volatile static boolean sNeedCalledApplication = true;
 
 }
