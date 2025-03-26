f7f91aa8311d3dd99bed2a40da4be8f73c886372
dadayabgodan <dadayabgodan@proton.me>
2025-03-27 01:28:48 +0300

    Add -o flag for output file path

--- dpt/src/main/java/com/luoye/dpt/Dpt.java
+++ dpt/src/main/java/com/luoye/dpt/Dpt.java
@@ -27,34 +27,36 @@ public class Dpt {
     }
 
     private static Apk parseOptions(String[] args){
-        Options options = new Options();
-        options.addOption(new Option(Const.OPTION_NO_SIGN_APK,Const.OPTION_NO_SIGN_APK_LONG,false,"Do not sign apk."));
-        options.addOption(new Option(Const.OPTION_DUMP_CODE,Const.OPTION_DUMP_CODE_LONG,false,"Dump the code item of DEX and save it to .json files."));
-        options.addOption(new Option(Const.OPTION_OPEN_NOISY_LOG,Const.OPTION_OPEN_NOISY_LOG_LONG,false,"Open noisy log."));
-        options.addOption(new Option(Const.OPTION_APK_FILE,Const.OPTION_APK_FILE_LONG,true,"Need to protect apk file."));
-        options.addOption(new Option(Const.OPTION_DEBUGGABLE,Const.OPTION_DEBUGGABLE_LONG,false,"Make apk debuggable."));
-        options.addOption(new Option(Const.OPTION_DISABLE_APP_COMPONENT_FACTORY,Const.OPTION_DISABLE_APP_COMPONENT_FACTORY_LONG,false,"Disable app component factory(just use for debug)."));
-        CommandLineParser commandLineParser = new DefaultParser();
-        try {
-            CommandLine commandLine = commandLineParser.parse(options, args);
-            if(!commandLine.hasOption(Const.OPTION_APK_FILE)){
-                usage(options);
-                return new Apk.Builder().build();
-            }
-            LogUtils.setOpenNoisyLog(commandLine.hasOption(Const.OPTION_OPEN_NOISY_LOG));
-
-
-            return new Apk.Builder()
-                    .filePath(commandLine.getOptionValue(Const.OPTION_APK_FILE))
-                    .sign(!commandLine.hasOption(Const.OPTION_NO_SIGN_APK))
-                    .debuggable(commandLine.hasOption(Const.OPTION_DEBUGGABLE))
-                    .appComponentFactory(!commandLine.hasOption(Const.OPTION_DISABLE_APP_COMPONENT_FACTORY))
-                    .dumpCode(commandLine.hasOption(Const.OPTION_DUMP_CODE))
-                    .build();
-        }
-        catch (ParseException e){
+    Options options = new Options();
+    options.addOption(new Option(Const.OPTION_NO_SIGN_APK,Const.OPTION_NO_SIGN_APK_LONG,false,"Do not sign apk."));
+    options.addOption(new Option(Const.OPTION_DUMP_CODE,Const.OPTION_DUMP_CODE_LONG,false,"Dump the code item of DEX and save it to .json files."));
+    options.addOption(new Option(Const.OPTION_OPEN_NOISY_LOG,Const.OPTION_OPEN_NOISY_LOG_LONG,false,"Open noisy log."));
+    options.addOption(new Option(Const.OPTION_APK_FILE,Const.OPTION_APK_FILE_LONG,true,"Need to protect apk file."));
+    options.addOption(new Option(Const.OPTION_DEBUGGABLE,Const.OPTION_DEBUGGABLE_LONG,false,"Make apk debuggable."));
+    options.addOption(new Option(Const.OPTION_DISABLE_APP_COMPONENT_FACTORY,Const.OPTION_DISABLE_APP_COMPONENT_FACTORY_LONG,false,"Disable app component factory(just use for debug)."));
+    options.addOption(new Option(Const.OPTION_OUTPUT_PATH,Const.OPTION_OUTPUT_PATH_LONG,true,"Output directory for protected apk."));
+    
+    CommandLineParser commandLineParser = new DefaultParser();
+    try {
+        CommandLine commandLine = commandLineParser.parse(options, args);
+        if(!commandLine.hasOption(Const.OPTION_APK_FILE)){
             usage(options);
+            return new Apk.Builder().build();
         }
+        LogUtils.setOpenNoisyLog(commandLine.hasOption(Const.OPTION_OPEN_NOISY_LOG));
+
+        return new Apk.Builder()
+                .filePath(commandLine.getOptionValue(Const.OPTION_APK_FILE))
+                .outputPath(commandLine.getOptionValue(Const.OPTION_OUTPUT_PATH))
+                .sign(!commandLine.hasOption(Const.OPTION_NO_SIGN_APK))
+                .debuggable(commandLine.hasOption(Const.OPTION_DEBUGGABLE))
+                .appComponentFactory(!commandLine.hasOption(Const.OPTION_DISABLE_APP_COMPONENT_FACTORY))
+                .dumpCode(commandLine.hasOption(Const.OPTION_DUMP_CODE))
+                .build();
+    }
+    catch (ParseException e){
+        usage(options);
+    }
 
         return new Apk.Builder().build();
     }
