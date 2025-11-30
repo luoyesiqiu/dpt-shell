2a3cdd09b04772bc79079e6a11494d3bfeef0bed
luoyesiqiu <9801756+luoyesiqiu@users.noreply.github.com>
2025-11-30 08:41:46 +0800

    Update logger

--- dpt/src/main/java/com/luoye/dpt/util/LogUtils.java
+++ dpt/src/main/java/com/luoye/dpt/util/LogUtils.java
@@ -13,42 +13,50 @@ public class LogUtils {
     private static volatile boolean openLog = true;
     private static volatile boolean openNoisyLog = false;
 
-    private enum LogType{
-        DEBUG,INFO,WARN,ERROR
+    public static final String ANSI_RESET = "\033[0m";
+    public static final String ANSI_RED = "\033[31m";
+    public static final String ANSI_GREEN = "\033[32m";
+    public static final String ANSI_YELLOW = "\033[33m";
+
+    private enum LogType {
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR
     }
 
-    public static void setOpenLog(boolean open){
+    public static void setOpenLog(boolean open) {
         openLog = open;
     }
 
-    public static void setOpenNoisyLog(boolean open){
+    public static void setOpenNoisyLog(boolean open) {
         openNoisyLog = open;
     }
 
-    public static void info(String fmt,Object... args){
-        println(LogType.INFO,TAG,String.format(Locale.US, fmt, args));
+    public static void info(String fmt, Object... args) {
+        println(LogType.INFO, TAG, String.format(Locale.US, fmt, args));
     }
 
-    public static void debug(String fmt,Object... args){
-        println(LogType.DEBUG,TAG,String.format(Locale.US, fmt, args));
+    public static void debug(String fmt, Object... args) {
+        println(LogType.DEBUG, TAG, String.format(Locale.US, fmt, args));
     }
 
-    public static void warn(String fmt,Object... args){
-        println(LogType.WARN,TAG,String.format(Locale.US, fmt, args));
+    public static void warn(String fmt, Object... args) {
+        println(LogType.WARN, TAG, String.format(Locale.US, fmt, args));
     }
 
-    public static void error(String fmt,Object... args){
-        println(LogType.ERROR,TAG,String.format(Locale.US, fmt, args));
+    public static void error(String fmt, Object... args) {
+        println(LogType.ERROR, TAG, String.format(Locale.US, fmt, args));
     }
 
-    public static void noisy(String fmt,Object... args){
-        if(openNoisyLog) {
+    public static void noisy(String fmt, Object... args) {
+        if (openNoisyLog) {
             println(LogType.INFO, TAG, String.format(Locale.US, fmt, args));
         }
     }
 
-    private static void println(LogType type,String tag, String msg) {
-        if(!openLog) {
+    private static void println(LogType type, String tag, String msg) {
+        if (!openLog) {
             return;
         }
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.US);
@@ -56,16 +64,18 @@ public class LogUtils {
         String timeOut = simpleDateFormat.format(new Date());
 
         msg = StringUtils.capitalizeFirstLetter(msg);
-        switch (type){
-            case DEBUG:
-            case INFO:
+        String color = "";
+        switch (type) {
             case WARN:
-                System.out.println(timeOut + "\t" + threadName + "\t" +  tag + "\t" + msg);
+                color = ANSI_YELLOW;
                 break;
             case ERROR:
-                System.err.println(timeOut + "\t" + threadName+ "\t" + tag + "\t" + msg);
+                color = ANSI_RED;
                 break;
         }
+
+        System.out.println(color + timeOut + "\t" + threadName + "\t" + tag + "\t" + msg + ANSI_RESET);
+
     }
 
 }
